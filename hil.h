913964d49c6aefb6f44609a69316a52f92c6bdee
Donghyun Gouk <kukdh1@gmail.com>
2017-08-22 10:31:24 +0900

    add copyright header to hil

--- hil.h
+++ hil.h
@@ -1,8 +1,23 @@
-/*
- * hil_new.h
+/**
+ * Copyright (C) 2017 CAMELab
  *
- *  Created on: Jul 13, 2016
- *      Author: zhang_000
+ * This file is part of SimpleSSD.
+ *
+ * SimpleSSD is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * SimpleSSD is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with SimpleSSD.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Authors: Jie Zhang <jie@camelab.org>
+ *          Donghyun Gouk <kukdh1@camelab.org>
  */
 
 #ifndef MEM_HIL_H_

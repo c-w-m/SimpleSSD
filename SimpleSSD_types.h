a5908fc0850eb41c4f5031584984fb722df10912
Donghyun Gouk <kukdh1@gmail.com>
2017-08-22 14:51:03 +0900

    remove non-standard types

--- SimpleSSD_types.h
+++ SimpleSSD_types.h
@@ -115,16 +115,6 @@ Type & Struct
 #define NSEC ((uint64)1000*USEC) // 1 000 000 000
 #define PSEC ((uint64)1000*NSEC) // 1 000 000 000 000
 
-typedef   signed char      sint8 ;
-typedef   signed short     sint16;
-typedef   signed int       sint32;
-typedef   signed long long sint64;
-
-typedef unsigned char      uint8 ;
-typedef unsigned short     uint16;
-typedef unsigned int       uint32;
-typedef unsigned long long uint64;
-
 //===== Address Sequence =====
 enum {
   ADDR_CHANNEL = 0,

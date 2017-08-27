8408f0a2f647f614e7e7aea052300ed5d0fadc79
Donghyun Gouk <kukdh1@gmail.com>
2017-08-27 18:23:48 +0900

    fix bug on MAX

--- ftl.cc
+++ ftl.cc
@@ -44,13 +44,15 @@ void FTL::initialize(){
 
 Tick FTL::read(Addr lpn, size_t npages, Tick arrived) {
   Tick finished = 0;
+  Tick oneReq;
   Addr ppn;
 
   ftl_statistics.read_req_arrive(arrived);
 
   for (size_t i = 0; i < npages; i++) {
     FTLmapping->read(lpn + i, ppn);
-    finished = MAX(readInternal(ppn, arrived), finished);
+    oneReq = readInternal(ppn, arrived);
+    finished = MAX(oneReq, finished);
   }
 
   Command cmd = Command(arrived, lpn, OPER_READ, param->page_byte * npages);
@@ -63,6 +65,7 @@ Tick FTL::read(Addr lpn, size_t npages, Tick arrived) {
 
 Tick FTL::write(Addr lpn, size_t npages, Tick arrived, bool init) {
   Tick finished = 0;
+  Tick oneReq;
   Addr ppn;
 
   ftl_statistics.write_req_arrive(arrived);
@@ -70,7 +73,8 @@ Tick FTL::write(Addr lpn, size_t npages, Tick arrived, bool init) {
   for (size_t i = 0; i < npages; i++) {
     FTLmapping->write(lpn + i, ppn);
     if (!init) {
-      finished = MAX(writeInternal(ppn, arrived), finished);
+      oneReq = writeInternal(ppn, arrived);
+      finished = MAX(oneReq, finished);
     }
   }
 
